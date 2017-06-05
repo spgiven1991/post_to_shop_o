006a52e4af386bbf6fbe0359e0de5b5b84370769
Samuel Given <spgiven1991@gmail.com>
2017-06-05 18:52:07 -0500

    Add Product Model and Products Controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517235759) do
+ActiveRecord::Schema.define(version: 20170605201626) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "user_prodcts", force: :cascade do |t|
     t.string   "product"
