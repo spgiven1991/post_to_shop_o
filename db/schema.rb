138db21c646220955ea202d1b7c13c57aac8d78e
Samuel Given <spgiven1991@gmail.com>
2017-06-06 13:35:04 -0500

    reboot

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,4 @@
 
 ActiveRecord::Schema.define(version: 20170605201626) do
 
-  create_table "products", force: :cascade do |t|
-    t.string   "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
